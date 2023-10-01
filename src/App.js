26072da8a02409e027239ffe8a6915a751456899
MueezHaider <smueezhaider@gmail.com>
2023-10-01 15:15:46 +0500

    all reducers are working correctly

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
 import Budget from './components/Budget';
-
 // Add code to import the other components here under
-
-
 import { AppProvider } from './context/AppContext';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import ExpenseItem from './components/ExpenseItem';
+import AllocationForm from './components/AllocationForm';
 const App = () => {
     return (
         <AppProvider>
@@ -17,25 +19,31 @@ const App = () => {
                     <div className='row mt-3'>
                         {
                             /* Add Budget component here */
+                            <Budget />
                         }        
 
                         {
+                            <Remaining/>
                             /* Add Remaining component here*/
                         }        
 
                         {
+                            <ExpenseTotal/>
                             /* Add ExpenseTotal component here */
                         }        
                        
                         {
+                            <ExpenseList/>
                             /* Add ExpenseList component here */
                         }         
 
                         {
+                        <ExpenseItem/>
                             /* Add ExpenseItem component here */
                         }        
 
                         {
+                        <AllocationForm/>
                             /* Add AllocationForm component here under */
                         }        
 
